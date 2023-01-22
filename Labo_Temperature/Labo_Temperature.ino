36b51e6139fa3d566e291e470097bfa28aa1db4f
NBrdy <noe.burdy@gmail.com>
2023-01-22 15:45:29 +0100

    backup

--- Labo_Temperature/Labo_Temperature.ino
+++ Labo_Temperature/Labo_Temperature.ino
@@ -1,9 +1,43 @@
-void setup() {
-  // put your setup code here, to run once:
+const int sensorPin = A0;
+const float baselineTemps = 20.0;
 
+
+void setup() {
+  Serial.begin(9600);  //ouvrir un port serie
+  for (int pinNumber = 2; pinNumber < 5; pinNumber++) {
+
+    pinMode(pinNumber, OUTPUT);
+    digitalWrite(pinNumber, LOW);
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
+void loop() {
+  int sensorVal = analogRead(sensorPin);
+  Serial.print("valeur capteur: ");
+  Serial.print(sensorVal);
+  float voltage = (sensorVal / 1024.0) * 5.0;
+  Serial.print(",volts: ");
+  Serial.print(voltage);
+  Serial.print(",degres c :");
+  float temperature = (voltage - .5) * 100;
+  Serial.println(temperature);
+  if (temperature < baselineTemps) {
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+  } else if (temperature > baselineTemps + 2 && temperature < baselineTemps + 4) {
+    digitalWrite(2, HIGH);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+  } else if (temperature > baselineTemps + 4 && temperature < baselineTemps + 6) {
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, LOW);
+  } else if (temperature >= baselineTemps + 6) {
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+  }
+  delay(100);
 }
