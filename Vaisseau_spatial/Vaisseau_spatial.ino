3c7b92ccb68ba15dc881c3b00627c86e93bac5b2
NBrdy <noe.burdy@gmail.com>
2023-01-22 14:23:56 +0100

    backup

--- Vaisseau_spatial/Vaisseau_spatial.ino
+++ Vaisseau_spatial/Vaisseau_spatial.ino
@@ -6,14 +6,16 @@ int const LED_JAUNE = 4;
 int const LED_BLEU = 6;
 int const BOUTON = 2;
 
+bool montee = true;
+int valeur_led_analogique = 0;
 
 void setup() {
   Serial.begin(9600);
 
   pinMode(LED_BLEU, OUTPUT);
-  //pinMode(LED_VERTE, OUTPUT);
-  // pinMode(LED_JAUNE, OUTPUT);
-  // pinMode(LED_ROUGE, OUTPUT);
+  pinMode(LED_VERTE, OUTPUT);
+  pinMode(LED_JAUNE, OUTPUT);
+  pinMode(LED_ROUGE, OUTPUT);
   pinMode(BOUTON, INPUT);
 }
 
@@ -32,21 +34,34 @@ void loop() {
   }
   ////////////////////////////////////////////////////////////////
 
-  if (etat_bouton == LOW) {
-    //pas dapuis sur le bouton
-    //digitalWrite(LED_VERTE, LOW);
-    //digitalWrite(LED_JAUNE, LOW);
-    //digitalWrite(LED_ROUGE, LOW);
-    
-    delay(100);
-  } else {  //apui sur le bouton
-            // digitalWrite(LED_VERTE, HIGH);
-            // delay(500);
-            // digitalWrite(LED_JAUNE, HIGH);
-            // delay(500);
+  //  Gestion LED Verte
+  analogWrite(LED_VERTE, valeur_led_analogique);
+
+  if(montee) valeur_led_analogique++;
+  if(!montee) valeur_led_analogique--;
+  
+  if(valeur_led_analogique == 255) montee = false;
+  if(valeur_led_analogique == 0) montee = true;
+
+
+
+  if (etat_bouton == HIGH) {  // bouton appuyé
+    // digitalWrite(LED_VERTE, HIGH);
+    // delay(500);
+    // digitalWrite(LED_JAUNE, HIGH);
+    // delay(500);
     //digitalWrite(LED_ROUGE, HIGH);
     //delay(500);
     //delay(1000);
     delay(100);
+
+  } else {  // bonton pas appuyé
+    //digitalWrite(LED_VERTE, LOW);
+    //pas dapuis sur le bouton
+    //digitalWrite(LED_VERTE, LOW);
+    //digitalWrite(LED_JAUNE, LOW);
+    //digitalWrite(LED_ROUGE, LOW);
+
+    delay(10);
   }
 }
